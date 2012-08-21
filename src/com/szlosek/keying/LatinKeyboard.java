63bc4f1fcd8a742e6cc8e9371bbac4028f90b3b4
Alan W Szlosek Jr <alan.szlosek@gmail.com>
2012-08-21 16:27:13 -0700

    indentation, swap 2 icons, start next round

--- src/com/szlosek/keying/LatinKeyboard.java
+++ src/com/szlosek/keying/LatinKeyboard.java
@@ -26,79 +26,78 @@ import android.view.inputmethod.EditorInfo;
 
 public class LatinKeyboard extends Keyboard {
 
-    private Key mEnterKey;
-    
-    public LatinKeyboard(Context context, int xmlLayoutResId) {
-        super(context, xmlLayoutResId);
-    }
+	private Key mEnterKey;
 
-    public LatinKeyboard(Context context, int layoutTemplateResId, 
-            CharSequence characters, int columns, int horizontalPadding) {
-        super(context, layoutTemplateResId, characters, columns, horizontalPadding);
-    }
+	public LatinKeyboard(Context context, int xmlLayoutResId) {
+	super(context, xmlLayoutResId);
+	}
 
-    @Override
-    protected Key createKeyFromXml(Resources res, Row parent, int x, int y, 
-            XmlResourceParser parser) {
-        Key key = new LatinKey(res, parent, x, y, parser);
-        if (key.codes[0] == 10) {
-            mEnterKey = key;
-        }
-        return key;
-    }
-    
-    /**
-     * This looks at the ime options given by the current editor, to set the
-     * appropriate label on the keyboard's enter key (if it has one).
-     */
-    void setImeOptions(Resources res, int options) {
-        if (mEnterKey == null) {
-            return;
-        }
-        
-        switch (options&(EditorInfo.IME_MASK_ACTION|EditorInfo.IME_FLAG_NO_ENTER_ACTION)) {
-            case EditorInfo.IME_ACTION_GO:
-                mEnterKey.iconPreview = null;
-                mEnterKey.icon = null;
-                mEnterKey.label = res.getText(R.string.label_go_key);
-                break;
-            case EditorInfo.IME_ACTION_NEXT:
-                mEnterKey.iconPreview = null;
-                mEnterKey.icon = null;
-                mEnterKey.label = res.getText(R.string.label_next_key);
-                break;
-            case EditorInfo.IME_ACTION_SEARCH:
-                mEnterKey.icon = res.getDrawable(
-                        R.drawable.sym_keyboard_search);
-                mEnterKey.label = null;
-                break;
-            case EditorInfo.IME_ACTION_SEND:
-                mEnterKey.iconPreview = null;
-                mEnterKey.icon = null;
-                mEnterKey.label = res.getText(R.string.label_send_key);
-                break;
-            default:
-                mEnterKey.icon = res.getDrawable(
-                        R.drawable.sym_keyboard_return);
-                mEnterKey.label = null;
-                break;
-        }
-    }
-    
-    static class LatinKey extends Keyboard.Key {
-        
-        public LatinKey(Resources res, Keyboard.Row parent, int x, int y, XmlResourceParser parser) {
-            super(res, parent, x, y, parser);
-        }
-        
-        /**
-         * Overriding this method so that we can reduce the target area for the key that
-         * closes the keyboard. 
-         */
-        @Override
-        public boolean isInside(int x, int y) {
-            return super.isInside(x, codes[0] == KEYCODE_CANCEL ? y - 10 : y);
-        }
-    }
+	public LatinKeyboard(Context context, int layoutTemplateResId, 
+	CharSequence characters, int columns, int horizontalPadding) {
+	super(context, layoutTemplateResId, characters, columns, horizontalPadding);
+	}
+
+	@Override
+	protected Key createKeyFromXml(Resources res, Row parent, int x, int y,XmlResourceParser parser) {
+		Key key = new LatinKey(res, parent, x, y, parser);
+		if (key.codes[0] == 10) {
+			mEnterKey = key;
+		}
+	return key;
+	}
+
+		/**
+		* This looks at the ime options given by the current editor, to set the
+		* appropriate label on the keyboard's enter key (if it has one).
+		*/
+	void setImeOptions(Resources res, int options) {
+		if (mEnterKey == null) {
+			return;
+		}
+
+		switch (options&(EditorInfo.IME_MASK_ACTION|EditorInfo.IME_FLAG_NO_ENTER_ACTION)) {
+			case EditorInfo.IME_ACTION_GO:
+				mEnterKey.iconPreview = null;
+				mEnterKey.icon = null;
+				mEnterKey.label = res.getText(R.string.label_go_key);
+				break;
+			case EditorInfo.IME_ACTION_NEXT:
+				mEnterKey.iconPreview = null;
+				mEnterKey.icon = null;
+				mEnterKey.label = res.getText(R.string.label_next_key);
+				break;
+			case EditorInfo.IME_ACTION_SEARCH:
+				mEnterKey.icon = res.getDrawable(
+				R.drawable.sym_keyboard_search);
+				mEnterKey.label = null;
+				break;
+			case EditorInfo.IME_ACTION_SEND:
+				mEnterKey.iconPreview = null;
+				mEnterKey.icon = null;
+				mEnterKey.label = res.getText(R.string.label_send_key);
+				break;
+			default:
+				mEnterKey.icon = res.getDrawable(
+				R.drawable.sym_keyboard_return);
+				mEnterKey.label = null;
+				break;
+		}
+	}
+
+	static class LatinKey extends Keyboard.Key {
+
+		public LatinKey(Resources res, Keyboard.Row parent, int x, int y, XmlResourceParser parser) {
+			super(res, parent, x, y, parser);
+		}
+
+		/**
+		* Overriding this method so that we can reduce the target area for the key that
+		* closes the keyboard. 
+		*/
+		@Override
+		public boolean isInside(int x, int y) {
+			return super.isInside(x, codes[0] == KEYCODE_CANCEL ? y - 10 : y);
+		}
+	}
 
 }
